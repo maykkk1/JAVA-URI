52cb2c0fd6d64cc9e28f7214d61b9670c681c819
maycon <maycon5591@gmail.com>
2022-05-20 21:59:15 -0300

    Bee1004

---
+++ Bee1004.java
@@ -0,0 +1,12 @@
+package bee;
+
+import java.util.Scanner;
+
+public class Bee1004 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int num1 = in.nextInt();
+		int num2 = in.nextInt();
+		System.out.println("PROD = " + (num1 * num2));
+	}
+}
